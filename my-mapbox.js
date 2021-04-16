a401d5c7f4e74e3317800c4196c9bb3cc908a727
MateusO97 <mat97_@hotmail.com>
2021-04-16 15:10:31 -0300

    Creating function to get searched location name

--- my-mapbox.js
+++ my-mapbox.js
@@ -9,12 +9,12 @@ mapboxgl.accessToken = 'pk.eyJ1IjoibWV0cmljb24iLCJhIjoiY2l3eTQxMWl3MDBmYTJ6cWg3Y
 var map = new mapboxgl.Map({
   container: 'map', // Container ID
   style: 'mapbox://styles/mapbox/streets-v11', // Map style to use
-  center: [-122.25948, 37.87221], // Starting position [lng, lat]
-  zoom: 12 // Starting zoom level
+  center: [-48.043782, -15.841599], // Starting position [lng, lat]
+  zoom: 10 // Starting zoom level
 });
 
 var marker = new mapboxgl.Marker() // Initialize a new marker
-  .setLngLat([-122.25948, 37.87221]) // Marker [lng, lat] coordinates
+  .setLngLat([-48.043782, -15.841599]) // Marker [lng, lat] coordinates
   .addTo(map); // Add the marker to the map
 
 var geocoder = new MapboxGeocoder({
@@ -23,6 +23,12 @@ var geocoder = new MapboxGeocoder({
   mapboxgl: mapboxgl, // Set the mapbox-gl instance
   marker: false, // Do not use the default marker style
   placeholder: 'Search for a place', // Placeholder text for the search bar
+  flyTo: {
+    easing: function(t) {
+      return t;
+    },
+    maxZoom: 8, // If you want your result not to go further than a specific zoom
+  }
 });
 
 // Add the geocoder to the map
@@ -30,51 +36,37 @@ map.addControl(geocoder);
 
 // After the map style has loaded on the page,
 // add a source layer and default styling for a single point
-map.on('load', function () {
 
-  map.addSource('coordinates', {
-    'type': 'geojson',
-    'data': {
-      'type': 'FeatureCollection',
-      'features': []
-    }
-  });
-
-
-  // Add a new layer to visualize the polygon.
-  map.addLayer({
-    'id': 'fillcity',
-    'type': 'fill',
-    'source': 'coordinates', // reference the data source
-    'layout': {},
-    'paint': {
-      'fill-color': '#0080ff', // blue color fill
-      'fill-opacity': 0.5
-    }
-  });
-  // Add a black outline around the polygon.
-  map.addLayer({
-    'id': 'outline',
-    'type': 'line',
-    'source': 'coordinates',
-    'layout': {},
-    'paint': {
-      'line-color': '#000',
-      'line-width': 3
-    }
-  });
-});
-
-  // Listen for the `result` event from the Geocoder // `result` event is triggered when a user makes a selection
-  //  Add a marker at the result's coordinates
-  geocoder.on('result', function (e) {
-    map.getSource('coordinates').setData(e.result.geometry);
-    document.getElementById("zip").innerHTML = JSON.stringify(e.result.context[0].text);
-    document.getElementById("address").innerHTML = JSON.stringify(e.result.text);
-    document.getElementById("city").innerHTML = JSON.stringify(e.result.context[1].text);
-    document.getElementById("state").innerHTML = JSON.stringify(e.result.context[2].text);
-    document.getElementById("country").innerHTML = JSON.stringify(e.result.context[3].text);
-    console.log(e.result);
-
-
-  });
+function get_location_name(geoData) {
+  // debugger;
+  var region, countryName, place, returnStr, district;
+  if(geoData.context){
+    $.each(geoData.context, function(i, v){
+      if(v.id.indexOf('place') >= 0) {
+        place = v.text;
+      }
+      if(v.id.indexOf('district') >= 0) {
+        place = v.text;
+      }
+      if(v.id.indexOf('region') >= 0) {
+        region = v.text;
+      }
+      if(v.id.indexOf('country') >= 0) {
+        countryName = v.text;
+      }
+    });
+  }
+  if(district) {
+    returnStr = district;
+  }
+  if(district == null) {
+    returnStr = place;
+  }
+  if(place == null) {
+    returnStr = region;
+  }
+  if (region == null) {
+    returnStr = geoData.place_name;
+  }
+  return returnStr;
+}
