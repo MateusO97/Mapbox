32a70794a8c337d854cfc1a1762133b84230cbba
MateusO97 <mat97_@hotmail.com>
2021-04-19 18:15:38 -0300

    Creating popups for markers

--- Leafythings/mapbox.js
+++ Leafythings/mapbox.js
@@ -3,8 +3,8 @@ mapboxgl.accessToken = 'pk.eyJ1IjoibWV0cmljb24iLCJhIjoiY2l3eTQxMWl3MDBmYTJ6cWg3Y
 var map = new mapboxgl.Map({
   container: 'map', // Container ID
   style: 'mapbox://styles/mapbox/light-v10',
-  center: [data[0].business.storeLongitude, data[0].business.storeLatitude], // Starting position [lng, lat]
-  zoom: 10 // Starting zoom level
+  center: [-79.69347, 44.377433], // Starting position [lng, lat]
+  zoom: 12 // Starting zoom level
 });
 
 map.addControl(new mapboxgl.FullscreenControl());
@@ -12,11 +12,34 @@ map.addControl(new mapboxgl.NavigationControl());
 
 
 map.on('load', function() {
-  
+
+console.log(data[0].business.storeLongitude);
+console.log(data[0].business.storeLatitude);
+console.log(data[0]);
+
  var stores = data;
  stores.forEach(function(store, i) {
    new mapboxgl.Marker()
    .setLngLat([store.business.storeLongitude,store.business.storeLatitude])
+   .setPopup(new mapboxgl.Popup({ offset: 25 }) // add popups
+    .setHTML(
+      '<div class="limbet-square">' +
+        '<img src="https://dz8osaahf9pd7.cloudfront.net/images/assets/featuredribbon.png" alt="View More Details" style="position:absolute; transform: translate(-20%,-13%);">' +
+        '<div class="limbet-img">' +
+          '<img src=' + store.storebizLogo + '>' +
+        '</div>' +
+        '<div class="limbet-header">' +
+          '<strong>' + store.business.storeName + '</strong>' +
+          '<div class="small">' +
+            '<strong>' + store.storebizLocation + '</strong>' +
+            '<br>' +
+            '<div>' +
+              '<span class="las la-star"></span>' + store.business.storervwAverage +
+            '</div>' +
+          '</div>' +
+        '</div>' +
+     '</div>'
+   ))
    .addTo(map);
  });
 
