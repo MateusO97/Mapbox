c68ab4a28ab50b00a6274c95640329b25ff71f18
MateusO97 <mat97_@hotmail.com>
2021-04-19 17:16:41 -0300

    Adding store markers

--- Leafythings/mapbox.js
+++ Leafythings/mapbox.js
@@ -2,29 +2,25 @@
 mapboxgl.accessToken = 'pk.eyJ1IjoibWV0cmljb24iLCJhIjoiY2l3eTQxMWl3MDBmYTJ6cWg3YmZtdjdsMSJ9.2vDbTw3ysscpy3YWkHo6aA';
 var map = new mapboxgl.Map({
   container: 'map', // Container ID
-  style: 'mapbox://styles/mapbox/streets-v11', // Map style to use
-  center: [-79.3849, 43.6529], // Starting position [lng, lat]
+  style: 'mapbox://styles/mapbox/light-v10',
+  center: [data[0].business.storeLongitude, data[0].business.storeLatitude], // Starting position [lng, lat]
   zoom: 10 // Starting zoom level
 });
 
 map.addControl(new mapboxgl.FullscreenControl());
 map.addControl(new mapboxgl.NavigationControl());
 
+
 map.on('load', function() {
-    map.addSource("point", {
-        "type": "geojson",
-        "data": {
-            "type": "FeatureCollection",
-            "features": test
-        }
-    });
-    map.addLayer({
-        "id": "point",
-        "type": "circle",
-        "source": "point",
-        "paint": {
-            "circle-radius": 8,
-            "circle-color": "#000"
-        }
-    });
+  
+ var stores = data;
+ stores.forEach(function(store, i) {
+   new mapboxgl.Marker()
+   .setLngLat([store.business.storeLongitude,store.business.storeLatitude])
+   .addTo(map);
+ });
+
+
+
+
 });
