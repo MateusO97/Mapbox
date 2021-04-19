26dd3bfe7c0d81a61325bd9b1bdc351afcebabce
MateusO97 <mat97_@hotmail.com>
2021-04-19 14:32:42 -0300

    Updating map controls

--- Leafythings/mapbox.js
+++ Leafythings/mapbox.js
@@ -1,5 +1,30 @@
 // Map via Mapbox GL
-L.mapbox.accessToken = 'pk.eyJ1IjoibWV0cmljb24iLCJhIjoiY2l3eTQxMWl3MDBmYTJ6cWg3YmZtdjdsMSJ9.2vDbTw3ysscpy3YWkHo6aA';
-var map = L.mapbox.map('map')
-    .setView([40, -74.50], 9)
-    .addLayer(L.mapbox.styleLayer('mapbox://styles/mapbox/streets-v11'));
+mapboxgl.accessToken = 'pk.eyJ1IjoibWV0cmljb24iLCJhIjoiY2l3eTQxMWl3MDBmYTJ6cWg3YmZtdjdsMSJ9.2vDbTw3ysscpy3YWkHo6aA';
+var map = new mapboxgl.Map({
+  container: 'map', // Container ID
+  style: 'mapbox://styles/mapbox/streets-v11', // Map style to use
+  center: [-79.3849, 43.6529], // Starting position [lng, lat]
+  zoom: 10 // Starting zoom level
+});
+
+map.addControl(new mapboxgl.FullscreenControl());
+map.addControl(new mapboxgl.NavigationControl());
+
+map.on('load', function() {
+    map.addSource("point", {
+        "type": "geojson",
+        "data": {
+            "type": "FeatureCollection",
+            "features": test
+        }
+    });
+    map.addLayer({
+        "id": "point",
+        "type": "circle",
+        "source": "point",
+        "paint": {
+            "circle-radius": 8,
+            "circle-color": "#000"
+        }
+    });
+});
