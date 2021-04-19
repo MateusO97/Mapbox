911aa232758d372f43c15e7943309d98bede68fd
MateusO97 <mat97_@hotmail.com>
2021-04-19 20:28:45 -0300

    Adding js and json to server

--- index.php
+++ index.php
@@ -1,1 +1,6 @@
-<?php include_once("Leafythings/index.html"); ?>
+<?php
+include_once("Leafythings/index.html");
+include_once("Leafythings/mapbox.js");
+include_once("Leafythings/Alliston.json");
+
+ ?>
