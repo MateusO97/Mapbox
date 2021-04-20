6726f8538873c36d0a8b6f29499496759696dcc2
MateusO97 <mat97_@hotmail.com>
2021-04-20 12:40:42 -0300

    Creating function to list all deals

--- mapbox.js
+++ mapbox.js
@@ -10,14 +10,73 @@ var map = new mapboxgl.Map({
 map.addControl(new mapboxgl.FullscreenControl());
 map.addControl(new mapboxgl.NavigationControl());
 
+var panel_element = document.getElementById('deal-panel');
+
+
+function openDealPanel() {
+  document.getElementById('deal-panel').style.display = "block";
+  document.getElementById('show-btn').style.display = "none";
+  document.getElementById('close-btn').style.display = "block";
+}
+
+function closePanel() {
+  document.getElementById('deal-panel').style.display = "none";
+  document.getElementById('show-btn').style.display = "block";
+  document.getElementById('close-btn').style.display = "none";
+}
+
+function buildDealList(data) {
+  var listings = document.getElementById('listings');
+  var listing = listings.appendChild(document.createElement('div'));
+  var today = new Date().getTime();
+  data.forEach(function(store, i){
+    if (store.business.storedeals.length > 0 && Date.parse(store.business.storedeals[0].dlsExpireDate) > today) {
+      //Create anchor to see deal
+      var link = listing.appendChild(document.createElement('a'));
+      link.href = store.storebizRoute;
+      link.style = "color: black";
+      // Create div with deal content
+      var dealColumn = link.appendChild(document.createElement('div'));
+      dealColumn.className = 'row pt-2 pt-md-2';
+      dealColumn.style = "line-height: 0";
+
+      // Create Div with deal images
+      var dealImage = dealColumn.appendChild(document.createElement('div'));
+      dealImage.className = 'col-md-5 col-5';
+
+      // Create image tag
+      var productImage = dealImage.appendChild(document.createElement('img'));
+      productImage.src = store.storebizLogo;
+      productImage.className = 'img-fluid';
+      productImage.style = 'max-widht: 10em';
+
+      // Create Div with deal informations
+      var dealInformation = dealColumn.appendChild(document.createElement('div'));
+      dealInformation.className = 'col-md col pt-3';
+
+      var dealTittle = dealInformation.appendChild(document.createElement('h4'));
+      dealTittle.className = 'bold pb-2';
+      dealTittle.innerHTML = store.business.storedeals[0].dlsName;
+
+      var dealParagraph = dealInformation.appendChild(document.createElement('p'));
+      dealParagraph.innerHTML = store.business.storedeals[0].dlsApplyTo;
+
+      var dealParagraph = dealInformation.appendChild(document.createElement('p'));
+      dealParagraph.style = 'line-height: normal';
+      dealParagraph.innerHTML = store.business.storeName;
+
+      var dealType = dealInformation.appendChild(document.createElement('p'));
+      dealType.innerHTML = store.business.storeplType;
+
+
+    }
+
+  });
+}
 
 map.on('load', function() {
-
-console.log(data[0].business.storeLongitude);
-console.log(data[0].business.storeLatitude);
-console.log(data[0]);
-
- var stores = data;
+var stores = data;
+console.log(stores);
  stores.forEach(function(store, i) {
    new mapboxgl.Marker()
    .setLngLat([store.business.storeLongitude,store.business.storeLatitude])
@@ -43,7 +102,7 @@ console.log(data[0]);
    .addTo(map);
  });
 
-
+ buildDealList(stores);
 
 
 });
